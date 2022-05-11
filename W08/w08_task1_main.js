96cc2bbb8d25a183c5a47333ff55921ea01d7409
okd002 <103481314+okd002@users.noreply.github.com>
2022-05-11 13:57:24 +0900

    commit

---
+++ W08/w08_task1_main.js
@@ -0,0 +1,104 @@
+d3.csv("https://okd002.github.io/InfoVis2022/W08/w08_task1.csv")
+    .then( data => {
+        data.forEach( d => { d.x = +d.x; d.y = +d.y; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 256,
+            height: 128,
+            margin: {top:40, right:10, bottom:20, left:60}
+        };
+
+        const scatter_plot = new ScatterPlot( config, data );
+        scatter_plot.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+class ScatterPlot {
+
+    constructor( config, data ) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || {top:10, right:10, bottom:10, left:10}
+        }
+        this.data = data;
+        this.init();
+    }
+
+    init() {
+        let self = this;
+
+        self.svg = d3.select( self.config.parent )
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+        self.xscale = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d.value)])
+            .range([0, inner_width]);
+  
+
+        self.yscale = d3.scaleLinear()
+            .domain(data.map(d => d.label))
+            .range([0, inner_height])
+            .paddingInner(0.1);
+
+        self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(5)
+            .tickSizeOuter(0);
+
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${inner_height})`)
+
+        self.yaxis = d3.axisLeft( self.yscale )
+            .tickSizeOuter(0);
+
+        self.yaxis_group = self.chart.append('g')
+
+    }
+
+    update() {
+        let self = this;
+
+        const space = 10;
+        const xmin = d3.min( self.data, d => d.x ) - space;
+        const xmax = d3.max( self.data, d => d.x ) + space;
+        self.xscale.domain( [xmin, xmax] );
+
+        const ymin = d3.min( self.data, d => d.y ) - space;
+        const ymax = d3.max( self.data, d => d.y ) + space;
+        self.yscale.domain( [ymin, ymax] );
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+        self.chart.selectAll("rect").data(data).enter()
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", d => yscale(d.label))
+        .attr("width", d => xscale(d.value))
+        .attr("height", yscale.bandwidth());
+
+        self.xaxis_group
+            .call( self.xaxis );
+
+        self.yaxis_group
+            .call( self.yaxis );
+    }
+}
+
+
+
+
