b64c057de9fed01d8aa26c984907502a38c4bb3c
okd002 <103481314+okd002@users.noreply.github.com>
2022-05-12 17:02:17 +0900

    commit

--- W08/task1_main.js
+++ W08/task1_main.js
@@ -1,12 +1,12 @@
 d3.csv("https://okd002.github.io/InfoVis2022/W08/w08_task1.csv")
     .then( data => {
-        data.forEach( d => { d.label = +d.label; d.value = +d.value;});
+        data.forEach( d => {d.value = +d.value;  });
 
         var config = {
             parent: '#drawing_region',
-            width: 256,
-            height: 128,
-            margin: {top:25, right:10, bottom:50, left:50},
+            width: 512,
+            height: 256,
+            margin: {top:25, right:50, bottom:50, left:50},
             title: 'Sample Data',
             xlabel: 'X label',
             ylabel: 'Y label'
